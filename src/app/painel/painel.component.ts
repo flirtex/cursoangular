c5da960d3c72658f24251ad0af4c9c6ea8f204e7
Thiago Vieira <thiago.developer.rj@gmail.com>
2019-11-01 17:56:07 -0300

    Primeiro Commit do Curso de Angular

---
+++ src/app/painel/painel.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Frase } from '../shared/frase.model'
+import { FRASES } from './frases-mock'
+
+@Component({
+  selector: 'app-painel',
+  templateUrl: './painel.component.html',
+  styleUrls: ['./painel.component.css']
+})
+export class PainelComponent implements OnInit {
+
+  public frases: Frase[] = FRASES
+  public instrucao: string = "Traduza a Frase"
+  public resposta: string
+
+  public rodada: number = 0
+  public rodadaFrase: Frase
+
+  public progresso: number = 0
+
+  constructor() { 
+    this.rodadaFrase = this.frases[this.rodada]
+    console.log(this.rodadaFrase) 
+  }
+
+  ngOnInit() {
+  }
+
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+    console.log(this.resposta)
+  }
+
+  verificarResposta(): void{
+  //trocar pergunta da rodada
+  if(this.rodadaFrase.frasePtBr == this.resposta){
+    alert('tradução correta')
+    this.rodada++
+
+    this.progresso += (100 / this.frases.length)
+    
+    this.rodadaFrase = this.frases[this.rodada]
+  }else{
+    alert('tradução errada')
+  }
+
+  }
+
+
+}
